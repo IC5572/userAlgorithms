9b45736b5e9768edca081a262339c7d6deb53101
jonspav <jon.spavin@spavin.co.uk>
2017-10-30 14:01:41 +0000

    Update JS1234.py

--- JS1234.py
+++ JS1234.py
@@ -6,14 +6,34 @@ now = datetime.now()
 
 UID = "JS1234"
 
-while 1 > 0:
-	now_time = now.time()
-	if now_time >= time(9,30) and now_time <=(9,35):				
-		LennyStocksAPI.buyStocks(UID, "AAPL", 2)
-		time.sleep(600)
-	if now_time >= time(15,30) and now_time <=(15,35):
-		LennyStocksAPI.sellStocks(UID, "AAPL", 2)
-		time.sleep(600)
+while True: 
+    n_time = datetime.datetime.now()
+    ''' print the hour '''
+    print (n_time.hour)
+    ''' print the min ''' 
+    print (n_time.minute)
+    
+    if n_time.hour >= 9 and n_time.minute >= 30:
+        if n_time.hour >=9 and n_time.minute <35:
+            LennyStocksAPI.buyStocks(UID, "ZOO", 2)
+            time.sleep(600)
+     
+    if n_time.hour >= 15 and n_time.minute >= 30:
+        if n_time.hour >=15 and n_time.minute <35:
+            LennyStocksAPI.buyStocks(UID, "ZOO", 2)
+            time.sleep(600)
+     
+
+
+
+#while 1 > 0:
+#	now_time = now.time()
+#	if now_time >= time(9,30) and now_time <=(9,35):				
+#		LennyStocksAPI.buyStocks(UID, "AAPL", 2)
+#		time.sleep(600)
+#	if now_time >= time(15,30) and now_time <=(15,35):
+#		LennyStocksAPI.sellStocks(UID, "AAPL", 2)
+#		time.sleep(600)
 	
 	
 
